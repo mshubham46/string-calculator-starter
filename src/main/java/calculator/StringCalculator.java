4d5d5dcc1f9669903b874dcb0f95a255d3d8a5c8
Shubham Mane <17.mshubham@gmail.com>
2021-10-16 17:12:19 +0530

    addition of two numbers delemited by comma

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,12 +2,17 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-    if(input.isEmpty()) {
-        return 0;
-    }else {
-    	return Integer.parseInt(input);
-    }
-    }
+	public int add(String input) {
+		if (input.isEmpty()) {
+			return 0;
+		} 
+		else if (input.contains(",")) {
+			String[] str = input.split(",");
+			return Integer.parseInt(str[0]) + Integer.parseInt(str[1]);
+		}
+		else {
+			return Integer.parseInt(input);
+		}
+	}
 
 }
