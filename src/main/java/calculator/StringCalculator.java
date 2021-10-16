1de2aa566411fdeda0017b78f2619535bc9d44cf
Shubham Mane <17.mshubham@gmail.com>
2021-10-16 13:00:39 +0530

    single number returned as integer

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,11 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    if(input.isEmpty()) {
         return 0;
+    }else {
+    	return Integer.parseInt(input);
+    }
     }
 
 }
